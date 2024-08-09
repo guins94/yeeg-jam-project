f06ab060b2d8f7d3093a29afcd13901073bccced
guins94 <guins94@hotmail.com>
2024-08-09 02:55:39 -0300

    References to Cinemachine Camera Added

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -5,15 +5,15 @@ using Cinemachine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager GameManagerInstance { get; protected set; } = null;
+    public static CinemachineVirtualCamera VirtualCameraInstance { get; protected set; } = null;
+    private CameraShake cameraShake = null;
+
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<CameraShake>().ShakeCamera(10, 5);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        GameManagerInstance = this;
+        VirtualCameraInstance = FindObjectOfType<CinemachineVirtualCamera>();
+        if (VirtualCameraInstance != null) cameraShake = VirtualCameraInstance.GetComponent<CameraShake>();
     }
 }
